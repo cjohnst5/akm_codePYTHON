fdaa39b8a4a4588aac34ae5a52aa424e284b1b8c
Carla Johnston <Carla Johnston>
2017-04-13 14:06:25 -0700

    The mergesoiSample only gets a 10 percent match rate. I'm going to modify the algorithm to be more lenient and wanted to preserve the original mergesoiSample function

--- make_data/make_data_main.py
+++ make_data/make_data_main.py
@@ -1,15 +1,29 @@
 # ----------------------------------------------------------
 # Import functions
 # ----------------------------------------------------------
-from unique_firms import *
+# from unique_firms import *
 from merge_soisample import *
 # ----------------------------------------------------------
 # Parameters
 # ----------------------------------------------------------
 ############################################################
 # Paths
+# Home desktop
+raw_data_path = "C:/Users/Daniel and Carla/Dropbox/projects/akm/dataRAW/"
+stata_data_path = "C:/Users/Daniel and Carla/Dropbox/projects/akm//dataSTATA"
+
+# School desktop
 raw_data_path = "C:/Users/carli/Dropbox/projects/akm/dataRAW/"
-stata_data_path = "../../dataSTATA"
+stata_data_path = "C:/Users/carli/Dropbox/projects/akm//dataSTATA"
+tables_path = "C:/Users/carli/Dropbox/projects/akm//tables"
+
+# # EML account
+# raw_data_path = "/scratch/public/carlajohnston/compDiff/dataRAW/"
+# figures_path = "/scratch/public/carlajohnston/compDiff/figures/"
+# stata_data_path = "/scratch/public/carlajohnston/compDiff/dataSTATA/"
+# code_stata_path = "/scratch/public/carlajohnston/compDiff/codeSTATA/"
+# tables_path = "/scratch/public/carlajohnston/compDiff/tables/"
+
 ############################################################
 # ****Arguments for unique_firms, merge_soisample
 match = 2010
@@ -19,10 +33,12 @@ tax_file = "10e0.csv"
 year = 2010
 ps=100
 
+# ****Arguments for merge_soisample
+practice = ''
 # ----------------------------------------------------------
 # Main program
 # ----------------------------------------------------------
 
 # unique_firms_func(ps, year, ey, raw_data_path, stata_data_path)
-merge_soisample_func(match, raw_data_path, stata_data_path, tax_file)
+merge_soisample_func(match, raw_data_path, stata_data_path, tables_path, tax_file, practice)
 
--- make_data/merge_soisample.py
+++ make_data/merge_soisample.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_extraction import DictVectorizer as DV
 
-def prep_wage_tax_data(match_year, raw_data, stata_data, irs_filename):
+def prep_wage_tax_data(match_year, raw_data, stata_data, irs_filename, practice):
     """
     This function matches the UI wage data with the IRS tax data for nonprofit firms. It saves the merged
     data as a csv file. it doesn't return anything.
@@ -19,17 +19,26 @@ def prep_wage_tax_data(match_year, raw_data, stata_data, irs_filename):
     :param stata_data: String. File path to the stata data folder.
     :param irs_filename: String. File name of the IRS data we need to use to match to the wage data. I'm using this
     the data filenames differ across years, so we can't specify the file name purely by knowing the match year.
+    :param practice: String. If "Yes" then only a toy version of the wage_data is created. If no, then all the wage data
+    is merged to macth with the IRS data.
+    :return: Returns two pandas dataframes. The first data frame is firm information for the non-profit firms from the
+    wage data in the match year specified. The second data frame is firm information for the non-profit firms from the
+    tax data in the match year specified.
     """
 
     # Importing the wage data for the specified match period
-    print "I'm inside"
     final_data = pd.DataFrame()
     for year in xrange(match_year, match_year+2):
         appended_data = pd.DataFrame()
         year_s = str(year)
-        directory = raw_data + "/Wages " + year_s + "/practice/"
+        if practice == "Yes":
+            directory = raw_data + "/Wages " + year_s + "/practice/"
+        else:
+            directory = raw_data + "/Wages " + year_s
+        print directory
         os.chdir(directory)
         for f in glob.glob("*.csv"):
+            print f
             # Read in data file
             df = pd.read_csv(f, dtype={'employ_id': str, 'worker_id': str, 'year': float, 'month': float, 'wages': float})
             # Dropping nonsense workers and missing values
@@ -78,7 +87,7 @@ def prep_wage_tax_data(match_year, raw_data, stata_data, irs_filename):
 
     # Merging firm level data with aggregated wage records and employment records
     florida_data = pd.concat([social, quarter_wages, total_emp], axis=1, join='inner')
-
+    florida_data['employ_id'] = florida_data.index
     # Keeping only nonprofits
     florida_data['legal'] = florida_data['legal'].map(lambda x: x.replace("C", "").strip(), na_action='ignore')
     florida_data = florida_data[florida_data['legal']=='Not-for-Profit']
@@ -95,8 +104,8 @@ def prep_wage_tax_data(match_year, raw_data, stata_data, irs_filename):
     name_fips_cw.loc[length2, 'fips'] = "0" + name_fips_cw.loc[length2, 'fips']
     name_fips_cw['fips'] = '12' + name_fips_cw['fips']
 
-    florida_data['county'] = florida_data['county'].map(lambda x: x.strip(), na_action='ignore')
-    florida_data['county'] = florida_data['county'] + " County"
+    florida_data.loc[:, 'county'] = florida_data['county'].map(lambda x: x.strip(), na_action='ignore')
+    florida_data.loc[:, 'county'] = florida_data['county'] + " County"
     florida_data = pd.merge(florida_data, name_fips_cw, how='outer', on='county')
 
     # Replacing missing fips codes
@@ -104,20 +113,30 @@ def prep_wage_tax_data(match_year, raw_data, stata_data, irs_filename):
     florida_data.loc[florida_data['county'] == 'Saint Johns County', 'fips'] = "12109"
     florida_data.loc[florida_data['county'] == 'Saint Lucie County', 'fips'] = "12111"
 
-    # Dropping data we don't need and getting county dummies
+    # Dropping data we don't need or that is missing
     florida_data.drop(['naics', 'legal', 'index', 'county'], axis=1, inplace=True)
     florida_data.dropna(inplace=True)
-    train_data = pd.get_dummies(florida_data, columns=['fips'])
-    train_data.drop(['ty1', 'ty2', 'ty3', 'ty4'], inplace=True, axis=1)
+    florida_data.reset_index(inplace=True, drop=True)
 
     # Importing IRS tax data
     tax_path = raw_data + "IRS/soiSample/Quant_data/temp/" + irs_filename
     tax_data = pd.read_csv(tax_path, dtype={'state': str, 'zip': str, 'tot_num_empls': float,
-                                            'comp_curr_ofcr_tot': float, 'oth_sal_wg_tot': float},
-                           usecols=['state', 'zip', 'tot_num_empls', 'comp_curr_ofcr_tot', 'oth_sal_wg_tot'])
+                                            'comp_curr_ofcr_tot': float, 'oth_sal_wg_tot': float, 'taxpd': str,
+                                            'ein': str},
+                           usecols=['state', 'zip', 'tot_num_empls', 'comp_curr_ofcr_tot', 'oth_sal_wg_tot',
+                                    'taxpd', 'ein'])
     tax_data = tax_data[tax_data['state'] == 'FL']
     tax_data['total_comp'] = tax_data['comp_curr_ofcr_tot'] + tax_data['oth_sal_wg_tot']
-    tax_data.rename(columns={'total_num_empls': 'total_emp'}, inplace=True)
+    tax_data.rename(columns={'tot_num_empls': 'total_emp'}, inplace=True)
+
+    # Recoding tax year
+    yr = str(match_year)[-2:]
+    yr1 = str(match_year+1)[-2:]
+
+    tax_data.loc[tax_data['taxpd'] == yr + '12', 'taxpd'] = '1'
+    tax_data.loc[tax_data['taxpd'] == yr1 + '03', 'taxpd'] = '2'
+    tax_data.loc[tax_data['taxpd'] == yr1 + '06', 'taxpd'] = '3'
+    tax_data.loc[tax_data['taxpd'] == yr1 + '09', 'taxpd'] = '4'
 
     # Importing crosswalk
     crosswalk_path = raw_data + "crosswalks/zcta5_county_florida.csv"
@@ -131,41 +150,115 @@ def prep_wage_tax_data(match_year, raw_data, stata_data, irs_filename):
     max_pop.drop('fips', inplace=True, axis=1)
     zip_county_cw = pd.merge(zip_county_cw, max_pop, how='inner', on=['zip', 'pop10'])
 
-    # Merging crosswalk with tax data and getting county dummies
+    # Merging crosswalk with tax data and dropping observations with missing data
     tax_data = pd.merge(tax_data, zip_county_cw, how='inner', on='zip')
     tax_data.drop(['comp_curr_ofcr_tot', 'oth_sal_wg_tot', 'state', 'zip', 'pop10'], inplace=True, axis=1)
-    test_data = pd.get_dummies(tax_data, columns=['fips'])
-    test_data.drop('total_comp', axis=1, inplace=True)
+    tax_data.dropna(inplace=True)
+    tax_data.reset_index(inplace=True, drop=True)
 
-    # Filling in missing county columns. Hmm. This isn't generalizable. I'll think about this more.
-    test_data['fips_12013'] = 0
-    test_data['fips_12023'] = 0
-    test_data['fips_12029'] = 0
-    test_data['fips_12037'] = 0
-    test_data['fips_12039'] = 0
-    test_data['fips_12041'] = 0
-    test_data['fips_12043'] = 0
-    test_data['fips_12047'] = 0
-    test_data['fips_12067'] = 0
-    test_data['fips_12075'] = 0
-    test_data['fips_12119'] = 0
-    test_data['fips_12123'] = 0
-    test_data['fips_12125'] = 0
-    test_data['fips_12131'] = 0
+    # Outputting to csv's
+    output_florida = raw_data + '/temp/' + 'florida_data_P' + practice + '.csv'
+    output_tax = raw_data + '/temp/' + 'tax_data_P' + practice + '.csv'
+    florida_data.to_csv(output_florida)
+    tax_data.to_csv(output_tax)
+    print "I'm done"
+    # return florida_data, tax_data
 
-    train_data['fips_12027'] = 0
-    train_data['fips_12077'] = 0
-    train_data['fips_12093'] = 0
+def nearest_neighbors(wage_data_csv, tax_data_csv, tables_path, practice):
+    """
+    :param wage_data: File path to wage_data csv file
+    :param tax_data: File path to tax_data csv file
+    :return:
+    """
 
-    test_data.sort_index(axis=1, inplace=True)
-    train_data.sort_index(axis=1, inplace=True)
+    # Importing the csv files into dataframes
+    wage_data = pd.read_csv(wage_data_csv, dtype={'ty1': float, 'ty2': float, 'ty3': float, 'ty4': float,
+                                                  'total_emp': float, 'employ_id': str, 'fips': str})
+    tax_data = pd.read_csv(tax_data_csv, dtype={'ein': str, 'taxpd': str, 'total_emp': float, 'total_comp': float,
+                                                  'fips': str})
+
+    # Putting an index column on wage and tax data
+    wage_data.columns = ['ind'] + wage_data.columns[1:].tolist()
+    tax_data.columns = ['ind'] + tax_data.columns[1:].tolist()
+
+    # Finding neighbors for first tax year firms in tax data
+    test_data = tax_data.loc[tax_data['taxpd']=='1', ['total_comp', 'total_emp']]
+    train_data = wage_data.loc[:, ['ty1', 'total_emp']]
+    nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(train_data)
+    distances, n1_ind = nbrs.kneighbors(test_data)
+    n1_ind = np.squeeze(n1_ind)
+
+    c1_match = wage_data.loc[np.squeeze(n1_ind), 'fips'].reset_index(drop=True) == tax_data.loc[tax_data['taxpd']=='1', 'fips'].reset_index(drop=True)
+    round1_ind = n1_ind[np.array(c1_match)]
+    wage_data['r1_bool'] = 0
+    wage_data.loc[round1_ind, 'r1_bool'] = 1
+
+    # Creating a dataframe with the matches
+    matches = pd.DataFrame()
+    matches = append_matches('1', c1_match, round1_ind, wage_data, tax_data, matches)
+    print "Done with round 1"
+    # Finding nearest neighbors for the third tax year firms in the data
+    train3ind = wage_data.loc[wage_data['r1_bool']==0, 'ind']
+    test_data = tax_data.loc[tax_data['taxpd']=='3', ['total_comp', 'total_emp']]
+    train_data = wage_data.loc[train3ind, ['ty3', 'total_emp']]
+
+    nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(train_data)
+    distances, n3_ind_temp = nbrs.kneighbors(test_data)
+    n3_ind = train3ind[np.squeeze(n3_ind_temp)]
+    c3_match = wage_data.loc[np.squeeze(n3_ind), 'fips'].reset_index(drop=True) == tax_data.loc[tax_data['taxpd']=='3', 'fips'].reset_index(drop=True)
+    round3_ind = n3_ind[np.array(c3_match)]
+    wage_data['r3_bool'] = 0
+    wage_data.loc[round3_ind, 'r3_bool'] = 1  #The column r3 is a one if the firm is a match to a firm in the tax data.
+
+    # Appending the matches
+    matches = append_matches('3', c3_match, round3_ind, wage_data, tax_data, matches)
+    print "Done with round3"
+    # Finding nearest neighbors for the fourth tax year firms in the data.
+    winners = wage_data['r1_bool'] + wage_data['r3_bool']
+    train4ind = wage_data.loc[winners == 0, 'ind']
+    test_data = tax_data.loc[tax_data['taxpd'] == '4', ['total_comp', 'total_emp']]
+    train_data = wage_data.loc[train4ind, ['ty4', 'total_emp']]
+
+    nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(train_data)
+    distances, n4_ind_temp = nbrs.kneighbors(test_data)
+    n4_ind = train4ind[np.squeeze(n4_ind_temp)]
+    c4_match = wage_data.loc[np.squeeze(n4_ind), 'fips'].reset_index(drop=True) == tax_data.loc[
+        tax_data['taxpd'] == '4', 'fips'].reset_index(drop=True)
+    round4_ind = n4_ind[np.array(c4_match)]
+    wage_data['r4_bool'] = 0
+    wage_data.loc[round4_ind, 'r4_bool'] = 1
+
+    # Appending the matches
+    matches = append_matches('4', c4_match, round4_ind, wage_data, tax_data, matches)
+    matches.columns = ['indexw'] + matches.columns[1:5].tolist() + ['indext'] + matches.columns[6:].tolist()
+    matches['tax_obs'] = tax_data.shape[0]
+    output_path = tables_path + '/merge_soisampleTable1_P' + practice + '.csv'
+    matches.to_csv(output_path)
+    print "Done with round 4"
+    return matches
 
 
+def append_matches(tax_year, county_vec, round_vec, wage_data, tax_data, matches):
+    """
+    :param tax_year: tax year is a string of length 1 that equals 1,2, 3, or 4.
+    :return: Appended data set of the matches
+    """
+    total_comp_str = 'ty' + tax_year
+    tax_matches = tax_data.loc[tax_data['taxpd'] == tax_year, ['fips', 'total_comp', 'total_emp', 'ein', 'taxpd']][np.array(county_vec)]
+    tax_matches.rename(columns={'fips': 'fipst', 'total_comp': 'total_compt', 'total_emp': 'total_empt'}, inplace=True)
+    wage_matches = wage_data.loc[round_vec, ['fips', total_comp_str, 'total_emp', 'employ_id']]
+    wage_matches.rename(columns={'fips': 'fipsw', total_comp_str: 'total_compw', 'total_emp': 'total_empw'}, inplace=True)
+    temp = pd.concat([wage_matches.reset_index(), tax_matches.reset_index()], axis=1)
+    matches = pd.concat([matches, temp], axis=0, ignore_index=True)
+    return matches
 
+# def analysis_ofmatches(output_file):
+#     TODO: Fill in this code. right now i'm going to do this quick and dirty in stata.
 
-
-
-    print "All done"
-
+def merge_soisample_func(match_year, raw_data, stata_data, tables_path, irs_filename, practice):
+    # prep_wage_tax_data(match_year, raw_data, stata_data, irs_filename, practice)
+    input_florida = raw_data + '/temp/' + 'florida_data_P' + practice + '.csv'
+    input_tax = raw_data + '/temp/' + 'tax_data_P' + practice + '.csv'
+    nearest_neighbors(input_florida, input_tax, tables_path, practice)
 
 
